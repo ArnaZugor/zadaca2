b29e27dad36b2d33ce59ceb347c3433087746e06
Arna Zugor <zugor.arna@gmail.com>
2023-03-25 11:49:40 +0100

    Izrada zadace2

--- src/Main.java
+++ src/Main.java
@@ -3,55 +3,55 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
-//        int numberA = 10;
-//        int numberB = 10;
-//
-//        if (numberA>numberB){
-//            System.out.println(numberA+numberB);
-//        } else if (numberB>numberA){
-//            System.out.println(numberB-numberA);
-//        } else {
-//            System.out.println(numberA*numberB);
-//        }
+        int numberA = 10;
+        int numberB = 10;
+
+        if (numberA>numberB){
+            System.out.println(numberA+numberB);
+        } else if (numberB>numberA){
+            System.out.println(numberB-numberA);
+        } else {
+            System.out.println(numberA*numberB);
+        }
 
 
-//        int taskTwoValueA = 12;
-//        int taskTwoValueB = 5;
-//
-//        if (taskTwoValueA>10 && taskTwoValueA%3 ==0 && taskTwoValueA<=10000){
-//            System.out.println("Is it possible to divide "+taskTwoValueA + " by " + taskTwoValueB + " :" + (taskTwoValueA%taskTwoValueB ==0));
-//        }
+        int taskTwoValueA = 12;
+        int taskTwoValueB = 5;
+
+        if (taskTwoValueA>10 && taskTwoValueA%3 ==0 && taskTwoValueA<=10000){
+            System.out.println("Is it possible to divide "+taskTwoValueA + " by " + taskTwoValueB + " :" + (taskTwoValueA%taskTwoValueB ==0));
+        }
 
 
-//        int lastTaskNumberA = 68;
-//        int lastTaskNumberB = 72;
-//        int lastTasknumberC = 4;
-//        int lastTasknumberD = 1;
-//
-//        if ((lastTaskNumberA+lastTaskNumberB>0) || (lastTasknumberC+lastTasknumberD>0)){
-//            int sumOfNumbers = lastTaskNumberA+lastTaskNumberB+lastTasknumberC+lastTasknumberD;
-//            System.out.println(sumOfNumbers);
-//            if (sumOfNumbers>100 && sumOfNumbers%2==0){
-//                System.out.println(sumOfNumbers/2);
-//            }else{
-//                if ((sumOfNumbers-1)%2==0){
-//                    System.out.println(sumOfNumbers/2);
-//                }else{
-//                    System.out.println("The number is still not divisible by 2");
-//                }
-//            }
-//        }
+        int lastTaskNumberA = 68;
+        int lastTaskNumberB = 72;
+        int lastTasknumberC = 4;
+        int lastTasknumberD = 1;
 
-        //int radiusOfCircle = 4;
-        //double areaOfCircle = Math.PI * (radiusOfCircle*radiusOfCircle);
-        //System.out.println(areaOfCircle);
+        if ((lastTaskNumberA+lastTaskNumberB>0) || (lastTasknumberC+lastTasknumberD>0)){
+            int sumOfNumbers = lastTaskNumberA+lastTaskNumberB+lastTasknumberC+lastTasknumberD;
+            System.out.println(sumOfNumbers);
+            if (sumOfNumbers>100 && sumOfNumbers%2==0){
+                System.out.println(sumOfNumbers/2);
+            }else{
+                if ((sumOfNumbers-1)%2==0){
+                    System.out.println(sumOfNumbers/2);
+                }else{
+                    System.out.println("The number is still not divisible by 2");
+                }
+            }
+        }
+
+        int radiusOfCircle = 4;
+        double areaOfCircle = Math.PI * (radiusOfCircle*radiusOfCircle);
+        System.out.println(areaOfCircle);
 
 
-//        int valueX =5;
-//        System.out.println((valueX+valueX+6)/2 - valueX);
-//
-//        int valueAmount = (valueX+valueX+6)/2 - valueX;
-//        System.out.println(valueAmount%2==0);
+        int valueX =5;
+        System.out.println((valueX+valueX+6)/2 - valueX);
+
+        int valueAmount = (valueX+valueX+6)/2 - valueX;
+        System.out.println(valueAmount%2==0);
 
 
         int novacUKasi = 100;
